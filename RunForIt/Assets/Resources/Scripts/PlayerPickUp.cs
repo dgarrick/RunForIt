0af82fca005607a22f38ccc333a0785a5fa6316d
Daniel Garrick <danieljamesgarrick@gmail.com>
2016-02-06 13:19:02 -0600

    project settings/assets include

---
+++ RunForIt/Assets/Resources/Scripts/PlayerPickUp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPickUp : MonoBehaviour {
+
+    // Use this for initialization
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("collision");
+        if (other.gameObject.name == "FlashLightInactive")
+        {
+            Destroy(other.gameObject);
+            other.gameObject.SetActive(false);
+        }
+    }
+}
