b26c9b994b7596cf45e8f8863c0d8e7da38f00c0
Daniel Garrick <danieljamesgarrick@gmail.com>
2016-02-06 16:36:18 -0600

    flashlights, batteries

--- RunForIt/Assets/Resources/Scripts/FlashlightScript.cs
+++ RunForIt/Assets/Resources/Scripts/FlashlightScript.cs
@@ -4,31 +4,45 @@ using System.Collections;
 public class FlashlightScript : MonoBehaviour
 {
 
-    private int charge;
+    private float charge;
     private Light beam;
     private bool isOn;
+    private bool isReallyOn;
     void Start()
     {
-        charge = 100;
+        charge = 40; //8 is max
         isOn = true;
+        isReallyOn = false;
         beam = gameObject.GetComponent("Light") as Light;
         InvokeRepeating("checkCharge", 0, 0.2f);
-        beam.intensity = charge;
+        beam.range = charge;
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("CLICKY CLICK");
             if (isOn)
             {
                 isOn = false;
-                beam.intensity = 0;
+                beam.range = 0;
             }
             else
             {
                 isOn = true;
+            }
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            isOn = false;
+            if (isReallyOn)
+            {
+                isReallyOn = false;
+                beam.range = 0;
+            }
+            else
+            {
+                isReallyOn = true;
             }
         }
     }
@@ -37,12 +51,19 @@ public class FlashlightScript : MonoBehaviour
     {
         if (isOn && charge > 0)
         {
-            charge -= 1;
-            beam.intensity = charge;
-            Debug.Log("OH NO");
+            charge -= 0.1f;
+            beam.range = Mathf.Pow(charge, 2);
+            beam.intensity = 3;
+        }
+        else if(isReallyOn && charge > 0)
+        {
+            charge -= 0.3f;
+            beam.range = Mathf.Pow(charge,2);
+            beam.intensity = 8;
         }
         if (charge == 0)
         {
+            beam.range = 0;
             beam.intensity = 0;
         }
     }
